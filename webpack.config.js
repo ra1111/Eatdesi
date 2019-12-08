9c64a103c9a867e3b7c856bb5032b59878aed6d8
ra1111 <rahula.raj2013@gmail.com>
2019-12-08 12:51:31 +0530

    aadded

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,17 @@ output: {       publicPath: '/',    path: resolve('dist'),filename: 'bundle.js' 
   
     {
       
-        test: /\.(png|jpg|gif|svg)$/,
-       use:[ {loader: 'file-loader?name=[name].[ext]',options: {
-        outputPath: 'images'
-      }
-       }]
+      test: /\.(gif|png|jpe?g|svg)$/i,
+      use: [
+        'file-loader',
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            bypassOnDebug: true, // webpack@1.x
+            disable: true, // webpack@2.x and newer
+          },
+        },
+      ],
     
   },
 {
