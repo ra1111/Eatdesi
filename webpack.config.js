75ae9d7db8f4ebaaa3bf5ffe638fb6804a7e90ae
ra1111 <rahula.raj2013@gmail.com>
2019-12-08 10:13:25 +0530

    aadded

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
+const resolve = require('path').resolve;
 module.exports = {  entry: './src/index.js',  
-output: {    path: __dirname + '/dist',    publicPath: '/',    filename: 'bundle.js'  },  devServer: {    contentBase: './dist',  },  module: {    rules: [    {      test: /\.(js|jsx)$/,      exclude: /node_modules/,      
+output: {       publicPath: '/',    path: resolve('dist'),filename: 'bundle.js'  },  devServer: {    contentBase: './dist',  },  module: {    rules: [    {      test: /\.(js|jsx)$/,      exclude: /node_modules/,      
     use: [{loader:'babel-loader' , options: {
     presets: [
       '@babel/preset-env',
