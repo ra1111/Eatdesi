7f1ccc4dd5fe9fdbc2738dfb647faae51ee79128
ra1111 <rahula.raj2013@gmail.com>
2019-12-23 22:04:08 +0530

    added redeem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { HashRouter } from "react-router-dom";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Content from './Content'
 
 import { BrowserRouter as Router } from 'react-router-dom';
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
+ReactDOM.render(<HashRouter basename={process.env.PUBLIC_URL}>< App /></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
