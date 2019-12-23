c255a942fbd8780c0fc1aaf83702c0a2b69c8414
ra1111 <rahula.raj2013@gmail.com>
2019-12-23 22:15:13 +0530

    removed relative path

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 import Content from './Content'
 
 import { BrowserRouter as Router } from 'react-router-dom';
-ReactDOM.render(<HashRouter basename={process.env.PUBLIC_URL}>< App /></HashRouter>, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
