fedbeb67854052092b7a7f276ba8607c5ae857b3
ra1111 <rahula.raj2013@gmail.com>
2019-12-23 22:51:34 +0530

    removed  head

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
 const resolve = require('path').resolve;
 
 module.exports = {  entry: './src/index.js', 
-devServer: {
-  historyApiFallback: true,
-   contentBase: './',
-   hot: true
-}, 
 output: {       publicPath: '/',    path: resolve('dist'),filename: 'bundle.js'  },  devServer: {    contentBase: './dist',  },  module: {    rules: [    {      test: /\.(js|jsx)$/,      exclude: /node_modules/,      
     use: [{loader:'babel-loader' , options: {
     presets: [
